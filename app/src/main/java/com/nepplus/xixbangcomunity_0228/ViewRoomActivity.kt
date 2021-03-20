abe55bf7356e776c6b431c2cea15fbf8c260c526
김종진 <martia0118@gmail.com>
2021-03-20 15:41:42 +0900

    방 상세 화면용 액티비티 추가

---
+++ app/src/main/java/com/nepplus/xixbangcomunity_0228/ViewRoomActivity.kt
@@ -0,0 +1,11 @@
+package com.nepplus.xixbangcomunity_0228
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewRoomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_room)
+    }
+}
