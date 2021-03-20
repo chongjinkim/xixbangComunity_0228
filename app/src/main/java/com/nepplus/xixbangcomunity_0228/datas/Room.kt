3eb645f75bc62672705016249738b0c45c7661f9
김종진 <martia0118@gmail.com>
2021-03-20 14:48:04 +0900

    방 데이터 표현 클래스 추가

---
+++ app/src/main/java/com/nepplus/xixbangcomunity_0228/datas/Room.kt
@@ -0,0 +1,7 @@
+package com.nepplus.xixbangcomunity_0228.datas
+
+class Room(val price : Int,
+           val address : String,
+           val floor : Int,
+           val description : String) {
+}
