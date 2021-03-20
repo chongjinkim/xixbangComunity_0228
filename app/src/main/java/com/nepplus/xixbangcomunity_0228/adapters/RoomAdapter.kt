9282035a1f46b8d4662b246344ed85cc10561802
김종진 <martia0118@gmail.com>
2021-03-20 15:39:38 +0900

    가격을 활용하는 기능 추가

--- app/src/main/java/com/nepplus/xixbangcomunity_0228/adapters/RoomAdapter.kt
+++ app/src/main/java/com/nepplus/xixbangcomunity_0228/adapters/RoomAdapter.kt
@@ -33,6 +33,10 @@ class RoomAdapter(
 
         descTxt.text = data.description
 
+        addressAndFloorTxt.text = "${data.address}, ${data.getFormattedFloor()}"
+
+        priceTxt.text = data.getFormattedPrice()
+
         return row
     }
 
--- app/src/main/java/com/nepplus/xixbangcomunity_0228/datas/Room.kt
+++ app/src/main/java/com/nepplus/xixbangcomunity_0228/datas/Room.kt
@@ -1,14 +1,36 @@
 package com.nepplus.xixbangcomunity_0228.datas
 
+import java.text.NumberFormat
+import java.util.*
+
 class Room(val price : Int,
            val address : String,
            val floor : Int,
            val description : String) {
 
+    fun getFormattedPrice() : String{
+
+        if(price < 10000){
+            val priceStr = NumberFormat.getNumberInstance(Locale.KOREA).format(this.price)
+            return priceStr
+        }
+
+        else{
+
+            val uk = this.price / 10000
+
+            val rest = this.price % 10000
+
+            val priceStr = "${uk}억${NumberFormat.getNumberInstance(Locale.KOREA).format(rest)}"
+
+            return priceStr
+        }
+    }
+
     fun getFormattedFloor() : String {
 
         if(this.floor >= 1){
-            return "$(this.floor)층"
+            return "${this.floor}층"
         }
 
         else if(this.floor == 0){
