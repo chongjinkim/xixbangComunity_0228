c24b3e98f51c148bea78159b28cca43f93d3a1aa
김종진 <martia0118@gmail.com>
2021-03-20 15:16:47 +0900

    층수를 가공해서 돌려주는 기능을 Room 클래스에 추가

--- app/src/main/java/com/nepplus/xixbangcomunity_0228/adapters/RoomAdapter.kt
+++ app/src/main/java/com/nepplus/xixbangcomunity_0228/adapters/RoomAdapter.kt
@@ -31,6 +31,7 @@ class RoomAdapter(
         val addressAndFloorTxt = row.findViewById<TextView>(R.id.addressAndFloorTxt)
         val descTxt = row.findViewById<TextView>(R.id.descTxt)
 
+        descTxt.text = data.description
 
         return row
     }
