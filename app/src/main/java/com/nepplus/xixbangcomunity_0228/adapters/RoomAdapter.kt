d272bcb52c983a890b378f5aa618155aafb5717d
김종진 <martia0118@gmail.com>
2021-03-20 14:56:29 +0900

    Adapter 뼈대 작업 2

--- app/src/main/java/com/nepplus/xixbangcomunity_0228/adapters/RoomAdapter.kt
+++ app/src/main/java/com/nepplus/xixbangcomunity_0228/adapters/RoomAdapter.kt
@@ -1,7 +1,11 @@
 package com.nepplus.xixbangcomunity_0228.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.nepplus.xixbangcomunity_0228.R
 import com.nepplus.xixbangcomunity_0228.datas.Room
 import java.util.ArrayList
 
@@ -9,4 +13,18 @@ class RoomAdapter(
     val mContext : Context,
     val resId : Int,
     val mList : ArrayList<Room>) : ArrayAdapter<Room>(mContext, resId, mList){
+
+    var inflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if(tempRow == null){
+            tempRow = inflater.inflate(R.layout.room_list_item,null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+
 }
